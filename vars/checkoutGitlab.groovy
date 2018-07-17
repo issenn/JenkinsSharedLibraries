de74d546562c9696e5470ba469627fd6ab433251
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-17 21:16:52 +0800

    Initial commit

---
+++ vars/checkoutGitlab.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+
+def call() {
+    checkout changelog: true, poll: true, scm: [
+        $class: 'GitSCM',
+        branches: scm.branches,
+        browser: [$class: 'GitLab', repoUrl: 'http://gitlab.hellotalk.com', version: '10.7'],
+        doGenerateSubmoduleConfigurations: false,
+        gitTool: 'git',
+        extensions: scm.extensions + [
+            [
+                $class: 'CloneOption',
+                depth: 0,
+                honorRefspec: true,
+                noTags: false,
+                reference: '',
+                shallow: true,
+                timeout: 10
+            ],
+            [
+                $class: 'CleanBeforeCheckout'
+            ],
+            [
+                $class: 'CleanCheckout'
+            ]
+        ],
+        submoduleCfg: scm.submoduleCfg,
+        userRemoteConfigs: scm.userRemoteConfigs
+    ]
+}
