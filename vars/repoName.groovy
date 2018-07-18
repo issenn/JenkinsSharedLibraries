51c68edb155714f8565af950ada82cb4829b25d7
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-18 20:36:28 +0800

    Initial commit

--- vars/repoName.groovy
+++ vars/repoName.groovy
@@ -3,9 +3,9 @@
 import java.net.URLDecoder;
 
 def call(script, String gitUrl='') {
-    gitUrl = gitUrl ?: script.env.GIT_URL
+    gitUrl = gitUrl ?: "${script.env.GIT_URL}"
     println(gitUrl)
-    println(script.env.GIT_URL)
+    println("${script.env.GIT_URL}")
     //def nameParts = (args.git_url - '.git').tokenize('/@')
     //for (def index = 0; index < nameParts.size(); index++) {
     //    nameParts[index] = URLDecoder.decode(nameParts[index], 'UTF-8')
