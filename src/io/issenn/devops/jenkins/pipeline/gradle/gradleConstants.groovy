4516ee03f5aff9501342e0c2ae9d9971f258842f
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-19 05:32:10 +0800

    Initial commit

--- src/io/issenn/devops/jenkins/pipeline/gradle/gradleConstants.groovy
+++ src/io/issenn/devops/jenkins/pipeline/gradle/gradleConstants.groovy
@@ -4,6 +4,8 @@ package io.issenn.devops.jenkins.pipeline.gradle
 
 class GradleConstants implements Serializable {
 
+    GradleConstants() {}
+
     def gradle(String command) {
         sh "set +x && ./gradlew ${command}"
     }
