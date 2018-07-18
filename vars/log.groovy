f3004f425eb2949ce340d20f3112f361b6d51a5d
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-19 02:41:43 +0800

    Initial commit

---
+++ vars/log.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
--- vars/pipelineEntry.groovy
+++ vars/pipelineEntry.groovy
@@ -4,8 +4,12 @@ import io.issenn.devops.jenkins.pipeline.environment.EnvironmentConstants
 
 def call() {
     if (env.CICD_TYPE == 'AndroidApp') {
+        println(pipelineAndroidApp)
+        log.info("pipelineAndroidApp")
         pipelineAndroidApp()
     } else if (env.CICD_TYPE == 'IosApp') {
+        println(pipelineIosApp)
+        log.info("pipelineIosApp")
         pipelineIosApp()
     } else {
         error "Don't know what to do with this CICD_TYPE: ${env.CICD_TYPE}"
