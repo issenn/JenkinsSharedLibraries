7f0ee8e1bc4aedd0d25ac54b776a30186d30b4ca
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-18 22:30:36 +0800

    Initial commit

--- vars/entry.groovy
+++ vars/entry.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
+import io.issenn.devops.jenkins.pipeline.environment.EnvironmentConstants
+
 def call(String repoNme='') {
     def repoName = libraryResource('repoName.json')
     println(repoName)
