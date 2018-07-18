c3c3b1151edf8265a153438a8957fd982c31758a
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-19 03:08:26 +0800

    Initial commit

--- vars/pipelineEntry.groovy
+++ vars/pipelineEntry.groovy
@@ -4,13 +4,13 @@ import java.text.SimpleDateFormat;
 import io.issenn.devops.jenkins.pipeline.environment.EnvironmentConstants
 
 def call() {
-    def dateFormat
-    def date
-    def formattedDate
 
     if (env.CICD_TYPE == 'AndroidApp') {
         log.info("entry pipelineAndroidApp")
         pipelineAndroidApp(
+            def dateFormat
+            def date
+            def formattedDate
             dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
             date = new Date()
             formattedDate = dateFormat.format(date)
