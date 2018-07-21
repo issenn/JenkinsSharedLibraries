8b013d3f9f4084b7e035c1b0cf07c3f5c2d8ffb3
issenn <issenn@issenndeMacBook-Pro.local>
2018-07-21 09:50:42 +0800

    Initial commit

--- vars/pipelineAndroidApp.groovy
+++ vars/pipelineAndroidApp.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 import org.gradle.api.invocation.Gradle
-import org.gradle.api.Project.getGradle
+import org.gradle.api.Project
 
 def call(Closure body={}) {
 
@@ -54,7 +54,7 @@ def call(Closure body={}) {
                         //println(environment.repoName(this))
                         println(environment.BRANCH_NAME)
                         println(environment.JOB_NAME)
-                        def gradle = getGradle()
+                        Gradle gradle = Project.getGradle()
                         println(gradle)
                     }
 
