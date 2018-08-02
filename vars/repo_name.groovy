980a40a4bf6a394f9f63244fe9a8fe88a2c6a8f9
issenn <issenn@hellotalk.com>
2018-08-02 09:58:24 +0800

    Initial commit

--- vars/repo_name.groovy
+++ vars/repo_name.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(Map args = [:]) {
-    println(env,GIT_URL)
     args = [git_url: env.GIT_URL] << args
-    println(args.git_url)
     def nameParts = (args.git_url - '.git').tokenize('/@')
     for (def index = 0; index < nameParts.size(); index++) {
         nameParts[index] = URLDecoder.decode(nameParts[index], 'UTF-8')
