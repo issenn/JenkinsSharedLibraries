0c461b830eee1cf2893b3a2f9d9b53d49ba80774
issenn <issenn@hellotalk.com>
2018-08-02 18:59:54 +0800

    Initial commit

--- vars/firPublish.groovy
+++ vars/firPublish.groovy
@@ -3,6 +3,6 @@
 def call(String path) {
     script {
         // sh "fir publish -T 9611b6a99d280463039cbb64b7eb24ca ${path}"
-        sh("fir publish -T db412ee63c68dcce0f9be05d0e40025c ${path}")
+        sh("sudo /usr/local/bin/firfir publish -T db412ee63c68dcce0f9be05d0e40025c ${path}")
     }
 }
