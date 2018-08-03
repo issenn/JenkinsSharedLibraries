6557716f16197effbcf5ccd897597d5637a4ee04
issenn <issenn@hellotalk.com>
2018-08-03 18:40:29 +0800

    Initial commit

--- vars/pipelineIosAppFeatureBranch.groovy
+++ vars/pipelineIosAppFeatureBranch.groovy
@@ -74,50 +74,6 @@ def call(Closure body={}) {
                     script {
                         def scmVars = checkoutGithub()
                         env.GIT_URL = scmVars.GIT_URL
- def changelogString = gitChangelog returnType: 'STRING',
-  from: [type: 'REF', value: 'git-changelog-1.50'],
-  to: [type: 'REF', value: 'master'],
-  template: """
-<h1> Git Changelog changelog </h1>
-
-<p>
-Changelog of Git Changelog.
-</p>
-
-{{#tags}}
-<h2> {{name}} </h2>
- {{#issues}}
-  {{#hasIssue}}
-   {{#hasLink}}
-<h2> {{name}} <a href="{{link}}">{{issue}}</a> {{title}} </h2>
-   {{/hasLink}}
-   {{^hasLink}}
-<h2> {{name}} {{issue}} {{title}} </h2>
-   {{/hasLink}}
-  {{/hasIssue}}
-  {{^hasIssue}}
-<h2> {{name}} </h2>
-  {{/hasIssue}}
-
-
-   {{#commits}}
-<a href="https://github.com/tomasbjerre/git-changelog-lib/commit/{{hash}}">{{hash}}</a> {{authorName}} <i>{{commitTime}}</i>
-<p>
-<h3>{{{messageTitle}}}</h3>
-
-{{#messageBodyItems}}
- <li> {{.}}</li>
-{{/messageBodyItems}}
-</p>
-
-
-  {{/commits}}
-
- {{/issues}}
-{{/tags}}
-"""
- currentBuild.description = changelogString
- println(currentBuild.description)
                     }
                 }
             }
@@ -296,6 +252,54 @@ Changelog of Git Changelog.
                     firPublish("${WORKSPACE}/build/IPA/${XCODE_CONFIGURATION}-${XCODE_SDK}/*.ipa")
                 }
             }
+        }
+        post {
+            script {
+ def changelogString = gitChangelog returnType: 'STRING',
+  from: [type: 'REF', value: 'git-changelog-1.50'],
+  to: [type: 'REF', value: 'master'],
+  template: """
+<h1> Git Changelog changelog </h1>
+
+<p>
+Changelog of Git Changelog.
+</p>
+
+{{#tags}}
+<h2> {{name}} </h2>
+ {{#issues}}
+  {{#hasIssue}}
+   {{#hasLink}}
+<h2> {{name}} <a href="{{link}}">{{issue}}</a> {{title}} </h2>
+   {{/hasLink}}
+   {{^hasLink}}
+<h2> {{name}} {{issue}} {{title}} </h2>
+   {{/hasLink}}
+  {{/hasIssue}}
+  {{^hasIssue}}
+<h2> {{name}} </h2>
+  {{/hasIssue}}
+
+
+   {{#commits}}
+<a href="https://github.com/tomasbjerre/git-changelog-lib/commit/{{hash}}">{{hash}}</a> {{authorName}} <i>{{commitTime}}</i>
+<p>
+<h3>{{{messageTitle}}}</h3>
+
+{{#messageBodyItems}}
+ <li> {{.}}</li>
+{{/messageBodyItems}}
+</p>
+
+
+  {{/commits}}
+
+ {{/issues}}
+{{/tags}}
+"""
+ currentBuild.description = changelogString
+ println(currentBuild.description)
+}
         }
     }
 }
