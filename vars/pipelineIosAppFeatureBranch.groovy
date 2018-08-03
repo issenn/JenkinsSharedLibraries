386074da8ed7573cf8c6394b1f9d0a421fde7960
issenn <issenn@hellotalk.com>
2018-08-03 18:53:58 +0800

    Initial commit

--- vars/pipelineIosAppFeatureBranch.groovy
+++ vars/pipelineIosAppFeatureBranch.groovy
@@ -84,6 +84,38 @@ def call(Closure body={}) {
 <p>
 Changelog of Git Changelog.
 </p>
+
+{{#tags}}
+<h2> {{name}} </h2>
+ {{#issues}}
+  {{#hasIssue}}
+   {{#hasLink}}
+<h2> {{name}} <a href="{{link}}">{{issue}}</a> {{title}} </h2>
+   {{/hasLink}}
+   {{^hasLink}}
+<h2> {{name}} {{issue}} {{title}} </h2>
+   {{/hasLink}}
+  {{/hasIssue}}
+  {{^hasIssue}}
+<h2> {{name}} </h2>
+  {{/hasIssue}}
+
+
+   {{#commits}}
+<a href="https://github.com/tomasbjerre/git-changelog-lib/commit/{{hash}}">{{hash}}</a> {{authorName}} <i>{{commitTime}}</i>
+<p>
+<h3>{{{messageTitle}}}</h3>
+
+{{#messageBodyItems}}
+ <li> {{.}}</li> 
+{{/messageBodyItems}}
+</p>
+
+
+  {{/commits}}
+
+ {{/issues}}
+{{/tags}}
   """
 
  currentBuild.description = changelogString
