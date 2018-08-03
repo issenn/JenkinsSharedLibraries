46faa4afa942da6911c5d3b946d587d960467a95
issenn <issenn@hellotalk.com>
2018-08-03 19:31:03 +0800

    Initial commit

--- vars/pipelineIosAppFeatureBranch.groovy
+++ vars/pipelineIosAppFeatureBranch.groovy
@@ -258,25 +258,3 @@ def call(Closure body={}) {
         }
     }
 }
-
-@NonCPS
-def getChangeString() {
-    MAX_MSG_LEN = 100
-    def changeString = ""
-
-    echo "Gathering SCM changes"
-    def changeLogSets = currentBuild.rawBuild.changeSets
-    for (int i = 0; i < changeLogSets.size(); i++) {
-        def entries = changeLogSets[i].items
-        for (int j = 0; j < entries.length; j++) {
-            def entry = entries[j]
-            truncated_msg = entry.msg.take(MAX_MSG_LEN)
-            changeString += " - ${truncated_msg}\n"
-        }
-    }
-
-    if (!changeString) {
-        changeString = " - No new changes"
-    }
-    return changeString
-}
