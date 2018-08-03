c5c4c523f162cdf163991c157623503e70f095a2
issenn <issenn@hellotalk.com>
2018-08-03 20:47:31 +0800

    Initial commit

--- vars/changelog.groovy
+++ vars/changelog.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String from, String to) {
     def changelogString = gitChangelog returnType: 'STRING',
-    from: [type: 'COMMIT', value: "${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}"],
-    to: [type: 'COMMIT', value: "${env.GIT_COMMIT}"],
+    from: [type: 'COMMIT', value: "${from}"],
+    to: [type: 'COMMIT', value: "${to}"],
     template: '''
 <h1> Git Changelog changelog </h1>
 Changelog of Git Changelog.
