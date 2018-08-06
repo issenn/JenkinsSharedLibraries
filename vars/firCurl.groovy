8befa8c67bce1d92f70844c8bf5ffe355030bd51
issenn <issenn@hellotalk.com>
2018-08-06 10:21:37 +0800

    Initial commit

--- vars/firCurl.groovy
+++ vars/firCurl.groovy
@@ -16,15 +16,15 @@ def call(String path, String type, String os) {
         println(key)
         println(token)
         println(env.CHANGELOG)
-        def cmd2 = """curl -F "key=${key}"              \
-            -F "token=${token}"             \
-            -F "file=@${path}"            \
-            -F "x:name=${bundleId}"             \
-            -F "x:version=${env.versionName}"         \
-            -F "x:build=${env.versionCode}"               \
-            -F "x:release_type=${type}"   \
-            -F "x:changelog='${env.CHANGELOG}'"       \
-            https://upload.qbox.me"""
+        def cmd2 = "curl -F \"key=${key}\"              \
+            -F \"token=${token}\"             \
+            -F \"file=@${path}\"            \
+            -F \"x:name=${bundleId}\"             \
+            -F \"x:version=${env.versionName}\"         \
+            -F \"x:build=${env.versionCode}\"               \
+            -F \"x:release_type=${type}\"   \
+            -F \"x:changelog='''${env.CHANGELOG}'''\"       \
+            https://upload.qbox.me"
         println(cmd2)
         def stdout2 = sh(returnStdout: true, script: "${cmd2}").trim()
         println(stdout2)
