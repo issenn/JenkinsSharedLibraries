55fb59be15a9374425cee13631567b4f65fe6264
issenn <issenn@hellotalk.com>
2018-08-06 10:07:54 +0800

    Initial commit

--- vars/firCurl.groovy
+++ vars/firCurl.groovy
@@ -4,13 +4,17 @@ import groovy.json.JsonSlurper
 
 def call(String path, String type, String os) {
     script {
-        api_token = "9611b6a99d280463039cbb64b7eb24ca"
-        json = "{\"type\":\"${os}\", \"bundle_id\":\"${env.bundleId}\", \"api_token\":\"${api_token}\"}"
-        cmd1 = "curl -X \"POST\" \"http://api.fir.im/apps\" \
+        def api_token = "9611b6a99d280463039cbb64b7eb24ca"
+        def json = "{\"type\":\"${os}\", \"bundle_id\":\"${env.bundleId}\", \"api_token\":\"${api_token}\"}"
+        def cmd1 = "curl -X \"POST\" \"http://api.fir.im/apps\" \
             -H \"Content-Type: application/json\" \
             -d  '${json}'"
-        cmd2 = """curl -F "key=db412ee63c68dcce0f9be05d0e40025c"              \
-            -F "token=LOvmia8oXF4xnLh0IdH05XMYpH6ENHNpARlmPc-T:TJUiJEdWnpgsptiQuUD7khx6PLY=:eyJzY29wZSI6InByby1hcHA6N2U2Nzc3NTI5OTg2YmNiNzQwN2MyNjdlODFhYjk4MmE0YjliNzY2YiIsImNhbGxiYWNrVXJsIjoiaHR0cDovL2FwaS5maXIuaW0vYXV0aC9xaW5pdS9jYWxsYmFjaz9wYXJlbnRfaWQ9NWI2MmFkYzY5NTlkNjk1MDNlM2Q3MjdhXHUwMDI2dGltZXN0YW1wPTE1MzMxOTQ1NjRcdTAwMjZzaWduPTNkZjcxXHUwMDI2dXNlcl9pZD01YjYyOWY4NjU0OGI3YTFhZGQ2YmYwZmYiLCJjYWxsYmFja0JvZHkiOiJrZXk9JChrZXkpXHUwMDI2ZXRhZz0kKGV0YWcpXHUwMDI2ZnNpemU9JChmc2l6ZSlcdTAwMjZmbmFtZT0kKGZuYW1lKVx1MDAyNm9yaWdpbj0kKHg6b3JpZ2luKVx1MDAyNm5hbWU9JCh4Om5hbWUpXHUwMDI2YnVpbGQ9JCh4OmJ1aWxkKVx1MDAyNnZlcnNpb249JCh4OnZlcnNpb24pXHUwMDI2aXNfdXNlX21xYz0kKHg6aXNfdXNlX21xYylcdTAwMjZjaGFuZ2Vsb2c9JCh4OmNoYW5nZWxvZylcdTAwMjZyZWxlYXNlX3R5cGU9JCh4OnJlbGVhc2VfdHlwZSlcdTAwMjZkaXN0cmlidXRpb25fbmFtZT0kKHg6ZGlzdHJpYnV0aW9uX25hbWUpXHUwMDI2c3VwcG9ydGVkX3BsYXRmb3JtPSQoeDpzdXBwb3J0ZWRfcGxhdGZvcm0pXHUwMDI2bWluaW11bV9vc192ZXJzaW9uPSQoeDptaW5pbXVtX29zX3ZlcnNpb24pXHUwMDI2dWlfcmVxdWlyZWRfZGV2aWNlX2NhcGFiaWxpdGllcz0kKHg6dWlfcmVxdWlyZWRfZGV2aWNlX2NhcGFiaWxpdGllcylcdTAwMjZ1aV9kZXZpY2VfZmFtaWx5PSQoeDp1aV9kZXZpY2VfZmFtaWx5KSIsImRlYWRsaW5lIjoxNTMzMTk4MTY0LCJ1cGhvc3RzIjpbImh0dHA6Ly91cC5xaW5pdS5jb20iLCJodHRwOi8vdXBsb2FkLnFpbml1LmNvbSIsIi1IIHVwLnFpbml1LmNvbSBodHRwOi8vMTgzLjEzMS43LjE4Il0sImdsb2JhbCI6ZmFsc2V9"             \
+        def stdout1 = sh(returnStdout: true, script: "${cmd1}").trim()
+        def slurper = new JsonSlurper().parseText(stdout1)
+        def key = slurper.cert.binary.key
+        def token = slurper.cert.binary.token
+        def cmd2 = """curl -F "key=${key}"              \
+            -F "token=${token}"             \
             -F "file=@${path}"            \
             -F "x:name=${bundleId}"             \
             -F "x:version=${env.versionName}"         \
@@ -18,12 +22,8 @@ def call(String path, String type, String os) {
             -F "x:release_type=${type}"   \
             -F "x:changelog=${env.CHANGELOG}"       \
             https://upload.qbox.me"""
-        aaa = sh(returnStdout: true, script: "${cmd1}").trim()
-        println(aaa)
-        def slurper = new JsonSlurper().parseText(aaa)
-        println(slurper.id)
-        println(slurper.cert.binary.key)
-        println(slurper.cert.binary.token)
+        def stdout2 = sh(returnStdout: true, script: "${cmd2}").trim()
+        println(stdout2)
         // bbb = sh(returnStdout: true, script: "${cmd2}").trim()
     }
 }
