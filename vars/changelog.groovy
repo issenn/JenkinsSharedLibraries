53952e79d03d67a4a4e87ff67112ee1ce46bfc2b
issenn <issenn@hellotalk.com>
2018-08-06 23:04:40 +0800

    Initial commit

--- vars/changelog.groovy
+++ vars/changelog.groovy
@@ -3,8 +3,8 @@
 def call(String from, String to) {
     println(from)
     println(to)
-    from = from == null ? to : from
-    println(from)
+    def aaa = from ?: '123'
+    println(aaa)
     def changelogString = gitChangelog returnType: 'STRING',
     from: [type: 'COMMIT', value: "${from}"],
     to: [type: 'COMMIT', value: "${to}"],
