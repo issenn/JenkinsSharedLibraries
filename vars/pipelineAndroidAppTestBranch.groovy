2e22be76368372faba0c5f36d5b59833f9a3e6b7
issenn <issenn@hellotalk.com>
2018-08-07 20:43:35 +0800

    Initial commit

--- vars/pipelineAndroidAppTestBranch.groovy
+++ vars/pipelineAndroidAppTestBranch.groovy
@@ -85,6 +85,12 @@ def call(Closure body={}) {
                                 steps {
                                     script {
                                         def scmVars = checkoutGithub()
+                                        // env.GIT_URL = scmVars.GIT_URL
+                                        // env.GIT_COMMIT = scmVars.GIT_COMMIT
+                                        // env.GIT_PREVIOUS_SUCCESSFUL_COMMIT = scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT
+                                        // env.CHANGELOG = changelog(GIT_PREVIOUS_SUCCESSFUL_COMMIT, GIT_COMMIT)
+                                        env.CHANGELOG = readFile file: "NEWS.md", encoding: "UTF-8"
+                                        println(env.CHANGELOG)
                                     }
                                 }
                             }
