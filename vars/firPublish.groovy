6f0dbb845c216fb12b93dd6af9ae89397de725c4
issenn <issenn@hellotalk.com>
2018-08-08 15:10:38 +0800

    Initial commit

--- vars/firPublish.groovy
+++ vars/firPublish.groovy
@@ -2,7 +2,7 @@
 
 def call(String path) {
     script {
-        sh "sudo /usr/local/bin/fir publish -T 9611b6a99d280463039cbb64b7eb24ca -c ${env.CHANGELOG} ${path}"
+        sh "sudo /usr/local/bin/fir publish -T 9611b6a99d280463039cbb64b7eb24ca -c '${env.CHANGELOG}' ${path}"
         // sh("sudo /usr/local/bin/fir publish -T db412ee63c68dcce0f9be05d0e40025c -c ${env.CHANGELOG} ${path}")
     }
 }
