0795a9d238f6b8b056545b3d3183cb46c52b5d81
issenn <issenn@hellotalk.com>
2018-08-09 18:30:43 +0800

    Initial commit

--- vars/androidFirPublish.groovy
+++ vars/androidFirPublish.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String path) {
+def call(String path, String changelog) {
     script {
-        sh "sudo /usr/local/bin/fir publish -T 9611b6a99d280463039cbb64b7eb24ca -c '${env.CHANGELOG}' ${path}"
-        // sh("sudo /usr/local/bin/fir publish -T db412ee63c68dcce0f9be05d0e40025c -c ${env.CHANGELOG} ${path}")
+        sh "sudo /usr/local/bin/fir publish -T 9611b6a99d280463039cbb64b7eb24ca -c '${changelog}' ${path}"
+        // sh("sudo /usr/local/bin/fir publish -T db412ee63c68dcce0f9be05d0e40025c -c ${changelog} ${path}")
     }
 }
