b51abef538160bb5daedd9ab3cd9ad77b0b7acaf
issenn <issenn@hellotalk.com>
2018-08-16 13:18:48 +0800

    Initial commit

--- vars/pipelineIosAppFeatureBranch.groovy
+++ vars/pipelineIosAppFeatureBranch.groovy
@@ -260,7 +260,7 @@ def call(Closure body={}) {
                     }
                     // firCurl("${WORKSPACE}/build/IPA/${XCODE_CONFIGURATION}-${XCODE_SDK}/${REPO_NAME}-${env.versionName}-${env.versionCode}.ipa", 'Adhoc', 'ios')
                     iosFirPublish("${WORKSPACE}/build/IPA/${XCODE_CONFIGURATION}-${XCODE_SDK}/*.ipa", env.changelog)
-                    // archiveArtifacts "build/Archives/Release-iphoneos/Release-iphoneos/HelloTalk_Binary.app.dSYM"
+                    archiveArtifacts "build/Archives/Release-iphoneos/Release-iphoneos/HelloTalk_Binary.app.dSYM"
                     archiveArtifacts '**/*.dSYM'
                 }
             }
