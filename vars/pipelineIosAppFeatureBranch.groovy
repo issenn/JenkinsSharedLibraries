6460569858a70cf8c3d83223b7b4d7aaee9187e3
issenn <issenn@hellotalk.com>
2018-08-16 20:07:44 +0800

    Initial commit

--- vars/pipelineIosAppFeatureBranch.groovy
+++ vars/pipelineIosAppFeatureBranch.groovy
@@ -72,8 +72,8 @@ def call(Closure body={}) {
                 steps {
                     script {
                         def scmVars = checkoutGithub()
-                        // def branchCode = gitVersioner.branchCode()
-                        // def news = readFile(file: "NEWS.md", encoding: "UTF-8")
+                        def branchCode = gitVersioner.branchCode()
+                        def news = readFile(file: "NEWS.md", encoding: "UTF-8")
                         env.changelog = "---+"  // + branchCode + news
                     }
                 }
