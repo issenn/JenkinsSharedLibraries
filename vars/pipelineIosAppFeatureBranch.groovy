6336ddba59c83f9a9cebc53ef1f84cafd0554865
issenn <issenn@hellotalk.com>
2018-10-12 18:15:22 +0800

    Initial commit

--- vars/pipelineIosAppFeatureBranch.groovy
+++ vars/pipelineIosAppFeatureBranch.groovy
@@ -85,7 +85,6 @@ def call(Closure body={}) {
 
 def buildTestBranch() {
     echo "Test branch - Build"
-    // sh 'bundle install'
-    // sh 'bundle update'
-    // sh 'bundle exec fastlane ios do_publish_all'
+    sh 'bundle install'
+    sh 'bundle exec fastlane ios do_publish_all'
 }
