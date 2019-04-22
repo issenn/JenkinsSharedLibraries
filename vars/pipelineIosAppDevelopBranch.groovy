c4af675d3879a2b66e35a0eda4b57bb564c53b37
issenn <issenn@issenn.com>
2019-04-22 11:04:04 +0800

    Initial commit

--- vars/pipelineIosAppDevelopBranch.groovy
+++ vars/pipelineIosAppDevelopBranch.groovy
@@ -106,6 +106,6 @@ def call(Closure body={}) {
 
 def buildDeveopBranch() {
     echo "Develop branch - Build"
-    sh 'bundle install'
+    sh 'bundle update --bundler && bundle install'
     sh 'bundle exec fastlane ios do_publish_all'
 }
