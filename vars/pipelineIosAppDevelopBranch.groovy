6fc2e3548f1bef185c19c9a6352ee9a9dc53a743
issenn <issenn@issenn.com>
2019-04-22 10:59:16 +0800

    Initial commit

--- vars/pipelineIosAppDevelopBranch.groovy
+++ vars/pipelineIosAppDevelopBranch.groovy
@@ -73,7 +73,7 @@ def call(Closure body={}) {
                     }
                 }
                 environment {
-                    PATH = "/usr/local/bin:${PATH}"
+                    PATH = "/usr/local/opt/ruby/bin:/usr/local/bin:${PATH}"
                 }
                 steps {
                     ansiColor('xterm') {
